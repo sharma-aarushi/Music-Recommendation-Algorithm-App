9692da4560ef97d67082ccfe6e3e5ac02b22a3c4
Aarushi Sharma <s.aarushi@aol.com>
2023-08-12 19:15:33 -0500

    Set up app.py and route for login page

---
+++ app.py/app.py
@@ -0,0 +1,38 @@
+import sys
+print(sys.executable)
+
+from flask import Flask, render_template
+from flask_login import LoginManager, UserMixin, login_required, login_user
+import os
+
+app = Flask(__name__)
+app.secret_key = 'my_own_secret_key_here' #TODO: Replace with more secure key
+
+# Simulated user data for testing
+# TODO: Replace this with a real database later
+users = {'testuser': {'password': 'testpassword'}}
+
+class User(UserMixin):
+    def __init__(self, username):
+        self.id = username
+
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+@login_manager.user_loader
+def load_user(username):
+    return User(username)
+
+#Route for the login page and mock login function:
+from flask import request, redirect, url_for
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        if users.get(username) and users[username]['password'] == password:
+            user = User(username)
+            login_user(user)
+            return redirect(url_for('profile'))
+    return render_template('login.html')
